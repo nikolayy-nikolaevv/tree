b69059a5a3719728d8ec806225a49304c44a5dcc
nikolayy-nikolaevv <nikolay.borislavov@gmail.com>
2016-02-23 23:54:11 +0200

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -8,19 +8,19 @@ int main()
    int n, a, i, j;
    cout<<"Enter n:";cin>>n;
      
-   for(j=1; j<=n-1; j++) cout<<" "; //отпечатва
-   cout<<"*"<<endl;                 //първия ред
+   for(j=1; j<=n-1; j++) cout<<" "; //prints
+   cout<<"*"<<endl;                 //the first line
    a=n-2;
    
    for (i=1; i<=n-2;i++)
    {
-    for(j=1;j<=a;j++) cout<<" "; //отпечатва интервалите преди "*"
-    for(j=1;j<=n-a;j++) cout<<"*"<<" "; //отпечатва "*" и интервалите вътре в дървото
-    cout<<endl; //нов ред
+    for(j=1;j<=a;j++) cout<<" "; //prints the spaces before "*"
+    for(j=1;j<=n-a;j++) cout<<"*"<<" "; //prints "*" and the spaces inside the tree
+    cout<<endl; //new line
     a-=1;
    }
   
-   for(j=1;j<=n;j++) cout<<"*"<<" "; //отпечатва последния ред
+   for(j=1;j<=n;j++) cout<<"*"<<" "; //prints the last line
    cout<<endl;
    
    
