d0ebc5f9668e527eadbd587e50ba34ae96506223
nikolayy_nikolaevv <nikolay.borislavov@gmail.com>
2022-11-26 18:12:21 +0100

    efficiency corrections

--- tree.cpp
+++ tree.cpp
@@ -1,31 +1,24 @@
 //tree.cpp
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 using namespace std;
 
 int main() 
 {
-   int n, a, i, j;
-   do {
-   cout<<"Enter n:";cin>>n;
-   } while (n<2); //prevents bad input
-   
-   for(j=1; j<=n-1; j++) cout<<" "; //prints
-   cout<<"*"<<endl;                 //the first line
-   a=n-2;
-   
-   for (i=1; i<=n-2;i++)
-   {
-    for(j=1;j<=a;j++) cout<<" "; //prints the spaces before "*"
-    for(j=1;j<=n-a;j++) cout<<"*"<<" "; //prints "*" and the spaces inside the tree
-    cout<<endl; //new line
-    a-=1;
-   }
-  
-   for(j=1;j<=n;j++) cout<<"*"<<" "; //prints the last line
-   cout<<endl;
-   
-   
-   system("PAUSE");
-   return 0;
+	int rows;
+	cout<<"Enter row:";
+	cin>>rows;
+	
+	for(int i = 1; i<=rows; i++) {
+		cout<<setw(rows-i+2);
+		for(int stars=1; stars<=i; stars++) {
+			cout<<" *";
+		}
+		cout<<endl;
+	}
+	
+	
+	system("PAUSE");
+	return 0;
 }
